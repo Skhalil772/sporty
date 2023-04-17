11a3beb7a879b9bbb33f30040df5ee2b203a0bb2
khalil <salamikhalil02@gmail.com>
2023-04-17 22:07:08 +0100

    grid image change

--- src/components/HomeGrid.jsx
+++ src/components/HomeGrid.jsx
@@ -1,12 +1,24 @@
+import Image from "next/image";
 import React from "react";
 
 function HomeGrid() {
 	return (
 		<div className="grid__image container mx-auto h-[30rem] my-10">
-			<div className="grid__image-1 bg-blue-400"> </div>
+			<div className="grid__image-1 bg-blue-400"></div>
 			<div className="grid__image-2 bg-green-400"> </div>
 			<div className="grid__image-3 bg-red-400"> </div>
-			<div className="grid__image-4 bg-gray-400"> </div>
+			<div className="grid__image-4 bg-gray-400 relative w-full ml-auto  ">
+				<Image
+					fill
+					cover
+					className="border-2 border-black"
+					alt="arsenal"
+					src="https://images.unsplash.com/photo-1551854304-9235bf86ef71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"></Image>{" "}
+				<div className="absolute inset-0 bg-black/50 p-20  text-white">
+					<p className="text-xl font-semibold">APPAREL</p>
+					<button className="mt-6 px-6 py-2 w-fit">SHOP NOW</button>
+				</div>
+			</div>
 		</div>
 	);
 }
